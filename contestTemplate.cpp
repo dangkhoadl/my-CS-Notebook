a4f73014db0536a5c178a3a20efb297494057d4c
dangkhoa1992 <dangledangkhoa@gmail.com>
2017-02-22 22:36:55 +0800

    Add contest template

---
+++ contestTemplate.cpp
@@ -0,0 +1,54 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <streambuf>
+#include <istream>
+#include <vector>
+#include <list>
+#include <forward_list>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <stack>
+#include <queue>
+#include <set>
+#include <iomanip>
+#include <sstream>
+#include <cmath>
+#include <time.h>
+#include <random>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+#include <utility>
+#include <climits>
+#include <cassert>
+#include <istream>
+#include <random>
+#include <iterator>
+#include <memory>
+#include <cctype>
+using namespace std;
+/*-------------------------------------------------------------------*/
+
+
+
+
+int main(int agrc, char *argv[]) {
+	freopen("..\\Debug\\Input.txt", "r", stdin);
+	std::ios::sync_with_stdio(false);
+/*-------------------------------------------------------------------*/
+
+
+
+
+
+
+	return 0;
+}
