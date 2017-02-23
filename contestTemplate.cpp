26eb0382ec2d1e526e7390b6844e356d883f2e1d
dangkhoa1992 <dangledangkhoa@gmail>
2017-02-23 14:24:02 +0800

    Update template

--- contestTemplate.cpp
+++ contestTemplate.cpp
@@ -35,21 +35,26 @@
 #include <memory>
 #include <cctype>
 using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+
+//parameters
+ll T;
 /*-------------------------------------------------------------------*/
+void solve() {
 
-
-
+}
 
 int main(int agrc, char *argv[]) {
 	freopen("..\\Debug\\Input.txt", "r", stdin);
-	//freopen("..\\Debug\\Output.txt", "w", stdout);
+	freopen("..\\Debug\\Output.txt", "w", stdout);
 	//std::ios::sync_with_stdio(false);
 /*-------------------------------------------------------------------*/
-
-
-
-
-
-
+	cin >> T;
+	for (int i = 1; i <= T; i++) {
+		
+		cout << "Case #" << i << ": " << solve() << endl;
+	}
 	return 0;
 }
