118a72aba0d7277b97669a351baa14e24efed3d2
dangkhoa1992 <dangledangkhoa@gmail.com>
2017-07-05 16:04:25 +0800

    Fixed templates

--- Templates/codeforces_template.cpp
+++ Templates/codeforces_template.cpp
@@ -53,72 +53,15 @@
 using namespace std;
 
 #define int long long
-#define double long double
 #define ll long long
 #define ull unsigned long long
-#define __builtin_popcount __builtin_popcountll
 
-#define REP(a) for(int iii=0,_a=(a); iii < _a; ++iii)
-#define FOR(iii,start,end,step) for(int iii=(start), _end=(end); iii < _end; iii += step) 				// [start, end)
-#define FORD(iii,start,end,step) for(int iii=(start), _end=(end); iii > _end; iii += step) 				// [start, end)
-#define for_i(start,end,step) for(int i = (start), _end(end), _step(step); i < (int)_end; i += _step) 	// [start, end)
-#define for_id(start,end,step) for(int i = (start), _end(end), _step(step); i > (int)_end; i += _step) 	// [start, end)
-#define for_j(start,end,step) for(int j = (start), _end(end), _step(step); j < (int)_end; j += _step) 	// [start, end)
-#define for_jd(start,end,step) for(int j = (start), _end(end), _step(step); j > (int)_end; j += _step) 	// [start, end)
-#define for_(start,end,step) for(int _ = (start), _end(end), _step(step); _ < (int)_end; _ += _step) 	// [start, end)
-#define for__(start,end,step) for(int __ = (start), _end(end), _step(step); __ < (int)_end; __ += _step)// [start, end)
-#define for_it(A) for(auto it = (A.begin()), _end(A.end()); it != _end; ++it)
-
-#ifdef DEBUG
-#define pr(args...) { vector<string> _v = split(#args, ','); err(_v.begin(), args); cerr << endl;}
-vector<string> split(const string& s, char c) {
-    vector<string> v;
-    stringstream ss(s);
-    string x;
-    while (getline(ss, x, c))
-        v.emplace_back(x);
-    return move(v);
-}
-void err(vector<string>::iterator it) {}
-template<typename T, typename... Args>
-void err(vector<string>::iterator it, T a, Args... args) {
-    cerr << it -> substr((*it)[0] == ' ', it->length()) << " = " << a << ", ";
-    err(++it, args...);
-}
-#define pra(A, start, end) { cerr << #A << '[' << start << ',' << (end - 1) << "] = "; for_(start, end, 1) cerr << A[_] << ' '; cerr << endl; }
-#define pra_(A, start, end) { cerr << #A << '[' << start << ',' << (end - 1) << "] = "; for_(start, end, 1) cerr << A[_] << ' '; cerr << " ; "; }
-#define prai(A, start, end) { cerr << #A << '[' << start << ',' << (end - 1) << "] = "; for_(start, end, 1) cerr << '[' << _ << ']' << A[_] << ' '; cerr << endl; }
-#define prai_(A, start, end) { cerr << #A << '[' << start << ',' << (end - 1) << "] = "; for_(start, end, 1) cerr << '[' << _ << ']' << A[_] << ' '; cerr << " ; "; }
-#define pra2(A, start1, end1, start2, end2) {for_(start1, end1, 1) {for__(start2, end2, 1) cerr << '[' << _ << "]["  << __ << ']' << A[_][__] << ' '; cerr << endl;} }
-void pravi(const vector<int> *a, int start, int end) {
-    for (int _ = start; _ < end; ++_) {
-        for (int __ = 0; __ < a[_].size(); ++__)
-            cerr << '[' << _ << "][" << __ << ']' << a[_][__] << "  ";
-        cerr << endl;
-    }
-}
-void prvvi(const vector<vector<int>> &a, int start, int end) {
-    for (int _ = start; _ < end; ++_) {
-        for (int __ = 0; __ < a[_].size(); ++__)
-            cerr << '[' << _ << "][" << __ << ']' << a[_][__] << "  ";
-        cerr << endl;
-    }
-}
-#else
-#define pr(args...)
-#define pra(A, start, end)
-#define pra_(A, start, end)
-#define prai(A, start, end)
-#define prai_(A, start, end)
-#define pra2(A, start1, end1, start2, end2)
-void pravi(const vector<int> *a, int start, int end) {}
-void prvvi(const vector<vector<int>> &a, int start, int end) {}
-#endif
-
+#define MIN LLONG_MIN
+#define MAX LLONG_MAX
 const int inf = 1e9 + 7;
+const double EPS = 1e-9;
 #define sq(x) ((x) * (x))
 #define PI (2 * acos((double)0))
-double safe_sqrt(double x) { return sqrt(max((double)0.0, x)); }
 
 typedef pair<int, int> pii;
 typedef vector<pair<int, int>> vii;
@@ -128,28 +71,68 @@ typedef vector<int> vi;
 typedef vector<vector<int>> vvi;
 #define pb push_back
 #define sz(a) (int)(a).size()
+#define ssz(a,start) (int)(strlen(a+start) + 1)
 #define bg(a) (a).begin()
 #define en(a) (a).end()
 #define all(a) (a).begin(), (a).end()
+#define ra(A, start, end) (A)+start, (A)+end
+#define rv(A, start, end) (A).begin()+start, (A).begin()+end
 #define ms(a,x) memset(a, x, sizeof(a)) //Care: Only init with 0,-1, char[] and bool(true,false)
 
-#define ra(A, start, end) (A)+start, (A)+end
-#define rvi(A, start, end) (A).begin()+start, (A).begin()+end
+#define REP(a) for(int iii=0, _a=(a); iii < _a; ++iii)
+#define FOR(iii,start,end) for(int iii=(start), _end=(end); iii < _end; ++iii)               // [start, end)
+#define FORD(iii,start,end) for(int iii=(start), _end=(end); iii > _end; --iii)              // [start, end)
+#define for_it(A) for(auto it = (A.begin()), _end(A.end()); it != _end; ++it)
+
 inline bool DEC(int iii_,int jjj_) { return (iii_<jjj_); }
+inline double safe_sqrt(double x) { return sqrt(max((double)0.0, x)); }
+inline const bool areSame(double a, double b) {return fabs(a - b) < EPS;}   
+inline int d2i(double x) {return x >= 0 ? x + EPS : x - EPS;}
+inline double sfloor(double x) {return x >= 0 ? floor(x + EPS) : floor(x - EPS);}
+inline double sceil(double x) {return x >= 0 ? ceil(x - EPS) : ceil(x + EPS);};
 
-const double EPSILON = 1e-6; // for x <= 10^6;
-inline const bool areSame(double a, double b) {return fabs(a - b) < EPSILON;}   
-inline int d2i(double x) {return x >= 0 ? x + EPSILON : x - EPSILON;}
-inline double sfloor(double x) {return x >= 0 ? floor(x + EPSILON) : floor(x - EPSILON);}
-inline double sceil(double x) {return x >= 0 ? ceil(x - EPSILON) : ceil(x + EPSILON);};
-/*----------------------------------- PARAMETERS -----------------------------------------------------------------*/
-const int MAXN = 1e6 + 1;
+#define bitcount __builtin_popcountll
+#define checkbit(n,b) ( (n >> b) & 1)
 
+#define si(nnn) scanf("%lld",&nnn)
+#define sii(nnn,mmm) scanf("%lld %lld",&nnn, &mmm)
+#define sd(ddd) scanf("%lf", &ddd)
+#define sc(ccc) scanf("%c", &ccc)
+#define ss(sss,iii) scanf("%s",sss+iii)
 
-void setClearPar() {
-	
-}
+#ifdef DEBUG
+template<typename T,typename TT> inline ostream& operator<<(ostream &s,pair<T,TT> t) {return s<<"("<<t.first<<","<<t.second<<")";}
+#define pr(args...) { vector<string> _v = split(#args, ','); err(_v.begin(), args); cerr << endl;}
+vector<string> split(const string& s, char c) {vector<string> v;stringstream ssss_(s);string x;while(getline(ssss_, x, c)) {v.emplace_back(x);}return move(v);}
+void err(vector<string>::iterator it) {}
+template<typename T, typename... Args> void err(vector<string>::iterator it, T a, Args... args) {cerr << it -> substr((*it)[0] == ' ', it->length()) << " = " << a << ", ";err(++it, args...);}
+#define pra(A, start, end) { cerr << #A << '[' << start << ',' << (end - 1) << "] = ";FOR(_,start, end) cerr << A[_] << ' '; cerr << endl; }
+#define pra_(A, start, end) { cerr << #A << '[' << start << ',' << (end - 1) << "] = ";FOR(_,start, end) cerr << A[_] << ' '; cerr << " ; "; }
+#define prai(A, start, end) { cerr << #A << '[' << start << ',' << (end - 1) << "] = ";FOR(_,start, end) cerr << '[' << _ << ']' << A[_] << ' '; cerr << endl; }
+#define prai_(A, start, end) { cerr << #A << '[' << start << ',' << (end - 1) << "] = ";FOR(_,start, end) cerr << '[' << _ << ']' << A[_] << ' '; cerr << " ; "; }
+#define pra2(A, start1, end1, start2, end2) {FOR(_,start1, end1) {FOR(__,start2, end2) cerr << '[' << _ << "]["  << __ << ']' << A[_][__] << ' '; cerr << endl;} }
+#define prit(A) {cerr << #A << " = ";for_it(A) {cerr << *it << ' ';}cerr << endl;}
+template<typename T> void prav(const vector<T> *a, int start, int end) {FOR(_,start,end) {FOR(__,0,sz(a[_])) {cerr << '[' << _ << "][" << __ << ']' << a[_][__] << "  ";} cerr << endl;}}
+template<typename T> void prvv(const vector<vector<T>> &a, int start, int end) {FOR(_,start,end) {FOR(__,0,sz(a[_])) {cerr << '[' << _ << "][" << __ << ']' << a[_][__] << "  ";}cerr << endl;}}
+template<typename T> void prS(const stack<T> &a) {stack<T> s(a);cerr << "stack =  [top] ";while(!s.empty()) {cerr << s.top() << ' ';s.pop();}cerr << endl;}
+template<typename T> void prQ(const queue<T> &a) {queue<T> q(a);cerr << "queue =  [front] ";while(!q.empty()) {cerr << q.front() << ' ';q.pop();}cerr << "[back]" << endl;}
+template<typename T> void prPQ(const priority_queue<T> &a) {priority_queue<T> pq(a);cerr << "heap =  [top] ";while(!pq.empty()) {cerr << pq.top() << ' ';pq.pop();}cerr << endl;}
+#else
+#define pr(args...)
+#define pra(A, start, end)
+#define pra_(A, start, end)
+#define prai(A, start, end)
+#define prai_(A, start, end)
+#define pra2(A, start1, end1, start2, end2)
+#define prit(A)
+template<typename T> void prav(const vector<T> *a, int start, int end) {}
+template<typename T> void prvv(const vector<vector<T>> &a, int start, int end) {}
+template<typename T> void prS(const stack<T> &a) {}
+template<typename T> void prQ(const queue<T> &a) {}
+template<typename T> void prPQ(const priority_queue<T> &a) {}
+#endif
 /*------------------------------------- FUNCTIONS ---------------------------------------------------------------*/
+const int MAXN = 1e6 + 1;
 
 
 ll readInput() {
@@ -176,10 +159,6 @@ int32_t main(int agrc, char *argv[]) {
 	cin.tie(false);
 	cout << fixed << setprecision(11);
 /*----------------------------------------------------------------------------------------------------*/
-	//Set/Clear Global parameter
-	setClearPar();
-
-
 	//InOut
 	readInput();
 
