c7e9fdfc91446f2432736daef932da32a091df2a
dangkhoa1992 <dangledangkhoa@gmail.com>
2017-07-14 19:34:06 +0800

    Added BFS sol KickStart2017_RoundC_ProbC

--- Contest_GoogleCodeJam/KickStart2017_RoundC/C.cpp
+++ Contest_GoogleCodeJam/KickStart2017_RoundC/C.cpp
@@ -1,83 +1,141 @@
 
 // https://code.google.com/codejam/contest/4344486/dashboard#s=p2
 
-const int MAXN = 2 + 1;
-const int MAXQ = 50 + 1;
+const int MAXN = 2 + 3;
+const int MAXQ = 50 + 3;
 
 int N, Q;
 char fr[MAXN][MAXQ];
 char me[MAXQ];
 int scr[MAXN];
 ll readInput() {
-    scanf("%lld %lld", &N, &Q);
-    FOR(n,1,N+1,1)
-        scanf("%s", fr[n] + 1);
-    scanf("%s", me + 1);
-    FOR(n,1,N+1,1)
-        scanf("%lld", &scr[n]);
+    sii(N,Q);
+    FOR(n,1,N+1)
+        ss(fr[n], 1);
+    ss(me,1);
+    FOR(n,1,N+1)
+        si(scr[n]);
     return 0;
 }
 
-int dp[MAXQ][MAXQ];
-int sol1() {
-    FOR(q,0,Q+1,1)
-        ms(dp[q], -1);
+/*______________________________________________ DP ___________________________________ */
+int dp1[MAXQ][MAXQ];
+int dp2[MAXQ][MAXQ][MAXQ];
+int dpSol() {
+    int m_,fr1_,fr2_;
+    if(N == 1) {
+        FOR(i,0,Q+1)
+            ms(dp1[i],-1);
 
-    dp[0][0] = 0;
-    FOR(q,0,Q,1) 
-        FOR(fr1,0,Q,1) {
-            if(dp[q][fr1] == -1)
+        dp1[0][0] = 0;
+        FOR(q,0,Q+1) FOR(fr1,0,Q+1) {
+            if(dp1[q][fr1] == -1)
                 continue;
 
-            //Case true
-            if(fr[1][q+1] == 'F' && me[q+1] == 'F')
-                dp[q+1][fr1] = max(dp[q+1][fr1], dp[q][fr1]);
-            else if (fr[1][q+1] == 'F' && me[q+1] == 'T')
-                dp[q+1][fr1] = max(dp[q+1][fr1], dp[q][fr1] + 1);
-            else if (fr[1][q+1] == 'T' && me[q+1] == 'F')
-                dp[q+1][fr1+1] = max(dp[q+1][fr1+1], dp[q][fr1]);
-            else if (fr[1][q+1] == 'T' && me[q+1] == 'T')
-                dp[q+1][fr1+1] = max(dp[q+1][fr1+1], dp[q][fr1] + 1);
+            //Case T
+            m_ = dp1[q][fr1];
+            fr1_ = fr1;
+            if(me[q+1] == 'T') ++m_;
+            if(fr[1][q+1] == 'T') ++fr1_;
+            dp1[q+1][fr1_] = max(dp1[q+1][fr1_], m_);
 
-            //Case false
-            if(fr[1][q+1] == 'F' && me[q+1] == 'F')
-                dp[q+1][fr1+1] = max(dp[q+1][fr1+1], dp[q][fr1] + 1);
-            else if (fr[1][q+1] == 'F' && me[q+1] == 'T')
-                dp[q+1][fr1+1] = max(dp[q+1][fr1+1], dp[q][fr1]);
-            else if (fr[1][q+1] == 'T' && me[q+1] == 'F')
-                dp[q+1][fr1] = max(dp[q+1][fr1], dp[q][fr1] + 1);
-            else if (fr[1][q+1] == 'T' && me[q+1] == 'T')
-                dp[q+1][fr1] = max(dp[q+1][fr1], dp[q][fr1]);
+            //Case F
+            m_ = dp1[q][fr1];
+            fr1_ = fr1;
+            if(me[q+1] == 'F') ++m_;
+            if(fr[1][q+1] == 'F') ++fr1_;
+            dp1[q+1][fr1_] = max(dp1[q+1][fr1_], m_);
         }
-    return dp[Q][scr[1]];
-}
+        return dp1[Q][scr[1]];
+    }
 
-int dp2[MAXQ][MAXQ][MAXQ];
-int fr1i, fr2i;
-int inc;
-int sol2() {
-    FOR(q,0,Q+1,1)
-        FOR(fr1,0,Q+1,1)
-            ms(dp2[q][fr1], -1);
+    FOR(i,0,Q+1) FOR(j,0,Q+1)
+        ms(dp2[i][j],-1);
 
     dp2[0][0][0] = 0;
-    FOR(q,0,Q,1) 
-        FOR(fr1,0,Q,1) 
-            FOR(fr2,0,Q,1) {
-                if(dp2[q][fr1][fr2] == -1)
-                    continue;
+    FOR(q,0,Q+1) FOR(fr1,0,Q+1) FOR(fr2,0,Q+1) {
+        if(dp2[q][fr1][fr2] == -1)
+            continue;
 
-                //Case true
-                fr1i = fr[1][q+1] == 'T' ? fr1+1 : fr1;
-                fr2i = fr[2][q+1] == 'T' ? fr2+1: fr2;
-                inc = me[q+1] == 'T' ? 1 : 0;
-                dp2[q+1][fr1i][fr2i] = max(dp2[q+1][fr1i][fr2i], dp2[q][fr1][fr2] + inc);
+        //Case T
+        m_ = dp2[q][fr1][fr2];
+        fr1_ = fr1;
+        fr2_ = fr2;
+        if(me[q+1] == 'T') ++m_;
+        if(fr[1][q+1] == 'T') ++fr1_;
+        if(fr[2][q+1] == 'T') ++fr2_;
+        dp2[q+1][fr1_][fr2_] = max(dp2[q+1][fr1_][fr2_], m_);
 
-                //Case false
-                fr1i = fr[1][q+1] == 'F' ? fr1+1 : fr1;
-                fr2i = fr[2][q+1] == 'F' ? fr2+1: fr2;
-                inc = me[q+1] == 'F' ? 1 : 0;
-                dp2[q+1][fr1i][fr2i] = max(dp2[q+1][fr1i][fr2i], dp2[q][fr1][fr2] + inc);
-            }
+        //Case F
+        m_ = dp2[q][fr1][fr2];
+        fr1_ = fr1;
+        fr2_ = fr2;
+        if(me[q+1] == 'F') ++m_;
+        if(fr[1][q+1] == 'F') ++fr1_;
+        if(fr[2][q+1] == 'F') ++fr2_;
+        dp2[q+1][fr1_][fr2_] = max(dp2[q+1][fr1_][fr2_], m_);
+    }
     return dp2[Q][scr[1]][scr[2]];
+}
+
+/*______________________________________________ BFS ___________________________________ */
+bool visited[MAXQ][MAXQ][MAXQ][MAXQ];
+queue<pair<pii,pii>> S;
+int bfsSol() {
+    int m_, fr1_,fr2_;
+    if(N == 1) 
+        scr[2] = 0;
+    while(!S.empty()) S.pop();
+    FOR(i,0,Q+1) FOR(j,0,Q+1) FOR(k,0,Q+1)
+        ms(visited[i][j][k], false);
+
+    S.push({{0,0},{0,0}});
+    visited[0][0][0][0] = true;
+
+    int res = -1;
+
+    while(!S.empty()) {
+        int q = S.front().fi.fi;
+        int m = S.front().fi.se;
+        int fr1 = S.front().se.fi;
+        int fr2 = (N == 1) ? 0 : S.front().se.se;
+        S.pop();
+
+        if(q > Q)
+            continue;
+
+        //process
+        if(fr1 == scr[1] && fr2 == scr[2])
+            res = max(res,m);
+
+        //Case T
+        m_ = m;
+        fr1_ = fr1;
+        fr2_ = fr2;
+        if(me[q+1] == 'T') ++m_;
+        if(fr[1][q+1] == 'T') ++fr1_;
+        if(fr[2][q+1] == 'T') ++fr2_;
+        if(!visited[q+1][m_][fr1_][fr2_]) {
+            visited[q+1][m_][fr1_][fr2_] = true;
+            S.push({{q+1,m_},{fr1_,fr2_}});
+        }
+
+        //Case F
+        m_ = m;
+        fr1_ = fr1;
+        fr2_ = fr2;
+        if(me[q+1] == 'F') ++m_;
+        if(fr[1][q+1] == 'F') ++fr1_;
+        if(fr[2][q+1] == 'F') ++fr2_;
+        if(!visited[q+1][m_][fr1_][fr2_]) {
+            visited[q+1][m_][fr1_][fr2_] = true;
+            S.push({{q+1,m_},{fr1_,fr2_}});
+        }
+    }
+    return res;
+}
+
+
+int solve() {
+    return bfsSol();
 }
