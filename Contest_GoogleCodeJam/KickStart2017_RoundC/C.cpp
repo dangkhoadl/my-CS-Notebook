2b729531b3d271b1aa0323c4679ccbde8d52dba1
dangkhoa1992 <dangledangkhoa@gmail.com>
2017-07-15 14:57:03 +0800

    Added BFS+DP sol

--- Contest_GoogleCodeJam/KickStart2017_RoundC/C.cpp
+++ Contest_GoogleCodeJam/KickStart2017_RoundC/C.cpp
@@ -132,7 +132,53 @@ int bfsSol() {
     return res;
 }
 
+/*____________________________ BFS + DP ___________________________________________*/
+int dp[MAXQ][MAXQ][MAXQ];
+queue<pair<int, pii>> X;
+int bfsDPSol() {
+    int m_, fr1_,fr2_;
+    if(N == 1) 
+        scr[2] = 0;
+    while(!X.empty()) X.pop();
+    ms(dp, -1);
+
+    X.push({0,{0,0}});
+    dp[0][0][0] = 0;
+    while(!X.empty()) {
+        int q = X.front().fi;
+        int fr1 = X.front().se.fi;
+        int fr2 = (N == 1) ? 0 : X.front().se.se;
+        int m = dp[q][fr1][fr2];
+        X.pop();
+
+        if(q > Q)
+            continue;
+
+        //Case T
+        m_ = m;
+        fr1_ = fr1;
+        fr2_ = fr2;
+        if(me[q+1] == 'T') ++m_;
+        if(fr[1][q+1] == 'T') ++fr1_;
+        if(N != 1 && fr[2][q+1] == 'T') ++fr2_;
+        if(dp[q+1][fr1_][fr2_] == -1)
+            X.push({q+1,{fr1_,fr2_}});
+        dp[q+1][fr1_][fr2_] = max(dp[q+1][fr1_][fr2_], m_);
+
+        //Case F
+        m_ = m;
+        fr1_ = fr1;
+        fr2_ = fr2;
+        if(me[q+1] == 'F') ++m_;
+        if(fr[1][q+1] == 'F') ++fr1_;
+        if(N != 1 && fr[2][q+1] == 'F') ++fr2_;
+        if(dp[q+1][fr1_][fr2_] == -1) 
+            X.push({q+1,{fr1_,fr2_}});
+        dp[q+1][fr1_][fr2_] = max(dp[q+1][fr1_][fr2_], m_);
+    }
+    return dp[Q][scr[1]][scr[2]];
+}
 
 int solve() {
-    return dpSol();
+    return bfsDPSol();
 }
