1b2e711fcea8e8b9d05e69d853850bf8d3cb55c2
dangkhoa1992 <dangledangkhoa@gmail.com>
2017-11-05 17:07:17 +0800

    Fix modulus

--- AlgorithmNotebook/Math/modulus.h
+++ AlgorithmNotebook/Math/modulus.h
@@ -1,4 +1,47 @@
 
+
+const long long MOD = 1e9+7;
+
+
+inline long long add(long long a, long long b) {
+    if(a<0)
+        a = (a%MOD) + MOD;
+    if(b<0)
+        b = (b%MOD) + MOD;
+    return ((a%MOD) + (b%MOD)) % MOD;
+}
+inline long long sub(long long a,long long b) {
+    if(a<0)
+        a = (a%MOD) + MOD;
+    if(b<0)
+        b = (b%MOD) + MOD;
+    return ((a%MOD) - (b%MOD) + MOD) % MOD;
+}
+inline long long mul(long long a,long long b) {
+    if(a<0)
+        a = (a%MOD) + MOD;
+    if(b<0)
+        b = (b%MOD) + MOD;
+    return ((a%MOD) * (b%MOD)) % MOD;
+}
+inline long long power(long long a, long long n) {
+    assert(n>0);
+    assert(a>=0);
+    if(a == 0)
+        return 0;
+    if(n == 0)
+        return 1;
+    if(n == 1)
+        return a%MOD;
+
+    long long pow_half = power(a, n/2);
+    long long pow_half_sq = (pow_half  * pow_half) % MOD;
+    if(n & 1)
+        return (pow_half_sq * a) % MOD;
+    return pow_half_sq;
+}
+
+
 inline long long gcd(long long a, long long b) {
     assert(a >= 0 and b >= 0 and a + b > 0);
 
@@ -23,43 +66,6 @@ inline vector<int> extendedGCD(int a, int b) {
     assert(a % dpq[0] == 0 and b % dpq[0] == 0);
     assert(dpq[0] == a * x + b * y);
     return {dpq[0],x,y};
-}
-
-const long long MOD = 1e9+7;
-inline long long add(long long a, long long b) {
-    if(a<0)
-        a = (a%MOD) + MOD;
-    if(b<0)
-        b = (b%MOD) + MOD;
-    return ((a%MOD) + (b%MOD)) % MOD;
-}
-inline long long sub(long long a,long long b) {
-    if(a<0)
-        a = (a%MOD) + MOD;
-    if(b<0)
-        b = (b%MOD) + MOD;
-    return ((a%MOD) - (b%MOD) + MOD) % MOD;
-}
-inline long long mul(long long a,long long b) {
-    if(a<0)
-        a = (a%MOD) + MOD;
-    if(b<0)
-        b = (b%MOD) + MOD;
-    return ((a%MOD) * (b%MOD)) % MOD;
-}
-inline long long power(long long a, long long b) {
-    assert(b>0);
-    assert(a>0);
-    if(a == 0)
-        return 0;
-    if(b == 0)
-        return 1;
-    if(b == 1)
-        return a%MOD;
-    long long halfb = power(a, b/2);
-    if(b & 1)
-        return (((halfb * halfb) % MOD ) * a ) % MOD;
-    return (halfb * halfb) % MOD;
 }
 inline long long inv(long long b) {
     if(b<0)
