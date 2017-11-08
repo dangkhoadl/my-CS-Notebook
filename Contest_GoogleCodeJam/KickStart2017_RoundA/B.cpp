7c90f86f8565a4fa0d4595edf8223264e091f484
dangkhoa1992 <dangledangkhoa@gmail.com>
2017-11-08 17:57:23 +0800

    Fix Kickstart 2017, prob B

--- Contest_GoogleCodeJam/KickStart2017_RoundA/B.cpp
+++ Contest_GoogleCodeJam/KickStart2017_RoundA/B.cpp
@@ -11,106 +11,133 @@ ll readInput() {
     return 0;
 }
 
-vector<char> S1;
-bool check1[20000];
-vector<char> S2;
-bool check2[20000];
-void preset() {
-    S1.clear();
-    S1.resize(1);
-    ms(check1,false);
+string x1, x2;
+void convert() {
+    x1 = "_";
     FOR(i,1,ssz(s1,1)) {
-        if(s1[i] != '*') {
-            S1.pb(s1[i]);
-            continue;
-        }
-        check1[sz(S1)] = true;
-        REP(4)
-            S1.pb('*');
+        if(s1[i] == '*')
+            x1 += "*+++";
+        else
+            x1 += s1[i];
     }
 
-    S2.clear();
-    S2.resize(1);
-    ms(check2,false);
+    x2 = "_";
     FOR(i,1,ssz(s2,1)) {
-        if(s2[i] != '*') {
-            S2.pb(s2[i]);
-            continue;
-        }
-        check2[sz(S2)] = true;
-        REP(4)
-            S2.pb('*');
+        if(s2[i] == '*')
+            x2 += "*+++";
+        else
+            x2 += s2[i];
     }
 }
-/*________________________________________________ DP ______________________________________*/
-bool dp[20000][20000];
-bool dpSol() {
-    preset();
-    ms(dp,false);
+
+
+/*________________________________________________ Sol1 __________________________________________ */
+bool dp[MAXN*4][MAXN*4];
+bool sol1() {
+    convert();
+
+    ms(dp, false);
 
     dp[1][1] = true;
-    FOR(x1,1,sz(S1)+1) FOR(x2,1,sz(S2)+1) {
-        if(!dp[x1][x2])
+
+    FOR(p1, 1, sz(x1)+1) FOR(p2, 1, sz(x2)+1) {
+        if(!dp[p1][p2])
             continue;
 
-        if(check1[x1]) {
-            FOR(x1_,x1+1,x1+5)
-                dp[x1_][x2] = true;
-        }
-        if(check2[x2]) {
-            FOR(x2_,x2+1,x2+5)
-                dp[x1][x2_] = true;
-        }
-        if(S1[x1] == S2[x2] || S1[x1] == '*' || S2[x2] == '*')
-            dp[x1+1][x2+1] = true;
-    }
-    return dp[sz(S1)][sz(S2)];
-}
-/*________________________________________________ BFS ______________________________________*/
-queue<pii> q;
-bool visited[20001][20001];
-bool bfsSol() {
-    preset();
-    while(!q.empty()) q.pop();
-    ms(visited,false);
+        int p1_, p2_;
 
+        // 1
+        if(x1[p1] == '*') {
+            FOR(i,1,5) {
+                p1_ = p1 + i;
+                p2_ = p2;
+                dp[p1_][p2_] = true;
+            }
+        }
+
+        // 2
+        if(x2[p2] == '*') {
+            FOR(i,1,5) {
+                p1_ = p1;
+                p2_ = p2 + i;
+                dp[p1_][p2_] = true;
+            }
+        }
+
+        // 3
+        if(x1[p1] == x2[p2] || x1[p1] == '+' || x2[p2] == '+' || x1[p1] == '*' || x2[p2] == '*') {
+            p1_ = p1 + 1;
+            p2_ = p2 + 1;
+            dp[p1_][p2_] = true;
+        }
+    }
+    return dp[sz(x1)][sz(x2)];
+}
+
+/*________________________________________________ Sol2 __________________________________________ */
+// bool dp[MAXN*4][MAXN*4];
+bool sol2() {
+    convert();
+
+    ms(dp, false);
+
+    queue<pii> q;
     q.push({1,1});
-    visited[1][1] = true;
+    dp[1][1] = true;
+
     while(!q.empty()) {
-        int x1 = q.front().fi;
-        int x2 = q.front().se;
+        int p1 = q.front().fi;
+        int p2 = q.front().se;
         q.pop();
 
-        //Process
-        if(x1 == sz(S1) && x2 == sz(S2))
-            return true;
+        int p1_, p2_;
+        bool check_;
 
-        if(check1[x1]) {
-            FOR(x1_,x1+1,x1+5) {
-                if(x1_ <= sz(S1)+1 && !visited[x1_][x2]) {
-                    visited[x1_][x2] = true;
-                    q.push({x1_,x2});
-                }
+        //1
+        if(x1[p1] == '*') {
+            FOR(i,1,5) {
+                p1_ = p1 + i;
+                p2_ = p2;
+
+                if(p1_ > sz(x1) or p2_ > sz(x2))
+                    continue;
+                if(!dp[p1_][p2_]) q.push({p1_,p2_});
+                dp[p1_][p2_] = true;
             }
         }
-        if(check2[x2]) {
-            FOR(x2_,x2+1,x2+5) {
-                if(x2_ <= sz(S2)+1 && !visited[x1][x2_]) {
-                    visited[x1][x2_] = true;
-                    q.push({x1,x2_});
-                }
+
+        //2
+        if(x2[p2] == '*') {
+            FOR(i,1,5) {
+                p1_ = p1;
+                p2_ = p2 + i;
+
+                if(p1_ > sz(x1) or p2_ > sz(x2))
+                    continue;
+                if(!dp[p1_][p2_]) q.push({p1_,p2_});
+                dp[p1_][p2_] = true;
             }
         }
-        if(S1[x1] == S2[x2] || S1[x1] == '*' || S2[x2] == '*') {
-            if(x1+1 <= sz(S1)+1 && x2+1 <= sz(S2)+1 && !visited[x1+1][x2+1]) {
-                visited[x1+1][x2+1];
-                q.push({x1+1,x2+1});
-            }
+
+        // 3
+        if(x1[p1] == x2[p2] || x1[p1] == '+' || x2[p2] == '+' || x1[p1] == '*' || x2[p2] == '*') {
+            p1_ = p1 + 1;
+            p2_ = p2 + 1;
+
+            if(p1_ > sz(x1) or p2_ > sz(x2))
+                continue;
+            if(!dp[p1_][p2_]) q.push({p1_,p2_});
+            dp[p1_][p2_] = true;
         }
     }
-    return false;
+    return dp[sz(x1)][sz(x2)];
 }
 
-int solve() {
-    return bfsSol();
+
+void solve(unsigned long long t) {
+    bool res = sol1();
+    if(res)
+        printf("Case #%llu: %s\n", t, "TRUE");
+    else
+        printf("Case #%llu: %s\n", t, "FALSE");
 }
