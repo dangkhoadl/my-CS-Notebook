66c3c29301f07f749e1b98cfcbaec8c0a7f0d225
dangkhoa1992 <dangledangkhoa@gmail.com>
2017-11-08 18:13:40 +0800

    Add DP

---
+++ AlgorithmNotebook/DP/Loop.h
@@ -0,0 +1,31 @@
+
+int dp[][];
+bool visited[][];
+int solveDP() {
+    ms(visited, false);
+    ms(dp, ); //0x7f, -0x7f, -1, 0
+    
+    FOR(i) FOR(j) {
+        if(!visited[i][j])
+            continue;
+        int i_, j_, dp_;
+
+        // Case 1
+        i_ = ;
+        j_ = ;
+        dp_ = ;
+        dp[i_][j_] = max(dp[i_][j_], dp_);
+        visited[i_][j_] = true;
+
+        // Case 2
+        i_ = ;
+        j_ = ;
+        dp_ = ;
+        dp[i_][j_] = max(dp[i_][j_], dp_);
+        visited[i_][j_] = true;
+    }
+
+    FOR()
+        res = max(res, );
+    return dp[][];
+}
