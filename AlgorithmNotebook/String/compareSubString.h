ce1f8b9a32571cb7df8883b5f43299b8f8c85314
dangkhoa1992 <dangledangkhoa@gmail.com>
2017-11-19 15:39:04 +0800

    Add rabinKarp

---
+++ AlgorithmNotebook/String/compareSubString.h
@@ -0,0 +1,15 @@
+
+
+// compare if 2 substrings of s1 and s2 are Equal
+// [start,end)
+bool cmpsubString(const string &s1, int start1, int end1, const string &s2, int start2, int end2) {
+    assert(start1 >= 0 and start1 < end1 and end1 <= s1.size());
+    assert(start2 >= 0 and start2 < end2 and end2 <= s2.size());
+    if((end1 - start1) != (end2 - start2))
+        return false;
+    for(int i = 0; i < (end1-start1); ++i) {
+        if(s1[i+start1] != s2[i+start2])
+            return false;
+    }
+    return true;
+}
--- AlgorithmNotebook/String/hashString.h
+++ AlgorithmNotebook/String/hashString.h
@@ -1,17 +1,18 @@
 
-// m = size of hashtable
+
+// Hash a string to integer. parameter p,x
 // Example , p = 1000000007
 // 			 x = 263
+// P small --> the int return likely be duplicated
 
-
-inline long long add(long long a, long long b,int MOD) {
+inline long long add(long long a, long long b,long long MOD) {
     if(a<0)
         a = (a%MOD) + MOD;
     if(b<0)
         b = (b%MOD) + MOD;
     return ((a%MOD) + (b%MOD)) % MOD;
 }
-inline long long mul(long long a,long long b, int MOD) {
+inline long long mul(long long a,long long b, long long MOD) {
     if(a<0)
         a = (a%MOD) + MOD;
     if(b<0)
@@ -34,9 +35,20 @@ inline long long power(long long a, long long n, long long MOD) {
         return (pow_half_sq * a) % MOD;
     return pow_half_sq;
 }
-long long hashString(string S, long long x, long long p, long long m) {
+
+
+long long hashString(const string &S, long long x, long long p) {
     long long num = 0;
     for(int i=0; i< S.size(); ++i)
         num = add(num,mul(S[i],power(x,i,p),p),p);
-    return num%m;
+    return num;
+}
+
+// [start,end)
+long long hashSubString(const string &S, int start, int end, long long x, long long p) {
+    assert(start >= 0 and end > start and end <= S.size());
+    long long num = 0;
+    for(int i = 0; i < (end-start); ++i)
+        num = add(num,mul(S[i+start],power(x,i,p),p),p);
+    return num;
 }
