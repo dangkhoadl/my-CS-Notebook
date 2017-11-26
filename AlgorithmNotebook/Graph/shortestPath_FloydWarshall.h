7735cb458d098a13305f968d6a7fecf3793ecb79
dangkhoa1992 <dangledangkhoa@gmail.com>
2017-11-26 23:47:34 +0800

    Fix FloydWarshall

--- AlgorithmNotebook/Graph/shortestPath_FloydWarshall.h
+++ AlgorithmNotebook/Graph/shortestPath_FloydWarshall.h
@@ -1,13 +1,16 @@
 
-//												shortest path by Floyd-Warshall
-// 	O(V^3)
+//                                              shortest path by Floyd-Warshall
+//  O(V^3)
 //
 // Floyd-Warshall gives us the shortest paths from all source to all target nodes. 
 // There are other uses for Floyd-Warshall as well;
 // it can be used to find connectivity in a graph (known as the Transitive Closure of a graph).
-// Note: No negative wei
-
+// Note: 
+//      - No negative wei
+//      - init ms(dist, 0x3f)
 // Dynamic Programming: f(i,j) = min(f(i,j), f(i,k) + f(k,j)): k = [1,V] is an intermediate node to cal dist(i,j)
+
+
 
 /*----------------------------------------- Floyd-Warshall for adjlist ---------------------------------------------------------------------------------------*/
 const int MAXV = 100 + 1;
@@ -17,30 +20,36 @@ vector<int> wei[MAXV];
 
 int dist[MAXV][MAXV];
 void floydWarshall() {
-	//Base cases
-	memset(dist, 0x7f, sizeof(dist));
-	for (int i = 1; i <= V; ++i) 
-		dist[i][i] = 0;
-		
-	for (int v = 1; v <= V; ++v)
-		for (int i = 0; i < e[v].size(); ++i)
-			dist[v][e[v][i]] = wei[v][i];
+    //Base cases
+    memset(dist, 0x3f, sizeof(dist));
+    for (int i = 1; i <= V; ++i) 
+        dist[i][i] = 0;
+        
+    for (int v = 1; v <= V; ++v)
+        for (int i = 0; i < e[v].size(); ++i)
+            dist[v][e[v][i]] = wei[v][i];
 
-	//DP
-	for (int k = 1; k <= V; ++k)
-		for (int i = 1; i <= V; ++i)
-			for (int j = 1; j <= V; ++j)
-				dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+    //DP
+    for (int k = 1; k <= V; ++k)
+        for (int i = 1; i <= V; ++i)
+            for (int j = 1; j <= V; ++j)
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
 }
 
 /*----------------------------------------- Floyd-Warshall for adjmatrix ---------------------------------------------------------------------------------------*/
+// Note: init ms(dist, 0x3f)
+//       Then update the dist[u][v] = d
 const int MAXV = 100 + 1;
 int V, E;
 
 int dist[MAXV][MAXV];
 void floydWarshall() {
-	for (int k = 1; k <= V; ++k)
-		for (int i = 1; i <= V; ++i)
-			for (int j = 1; j <= V; ++j)
-				dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+    for (int k = 1; k <= N; ++k)
+        for (int i = 1; i <= N; ++i)
+            for (int j = 1; j <= N; ++j)  {
+                if(i == j)
+                    dist[i][j] = 0;
+                else
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
 }
