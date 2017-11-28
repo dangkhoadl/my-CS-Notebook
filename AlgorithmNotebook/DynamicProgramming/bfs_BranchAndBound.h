3d8b3fe279701ce021b7a73d5cbbe060447c7197
dangkhoa1992 <dangledangkhoa@gmail.com>
2017-11-29 00:10:16 +0800

    Fix Branch and Bound

--- AlgorithmNotebook/DynamicProgramming/bfs_BranchAndBound.h
+++ AlgorithmNotebook/DynamicProgramming/bfs_BranchAndBound.h
@@ -4,60 +4,65 @@
 bool visited[][];
 int dp[][];
 int bfs() {
-	// Reset 
-	ms(visited, false);
-	ms(dp, ); //0x7f, -0x7f, -1, 0
+    ms(visited, false);
+    ms(dp, );
 
-	// Init 1st stage
-	queue<pii> q;
-	q.push( { 0,0 } );
-	visited[0][0] = true;
-	dp[0][0] = ;
-	while(!q.empty()) {
-		// current state
-		int a = q.front().fi;
-		int b = q.front().se;
-		int x = dp[a][b];
-		q.pop();
+    queue<pii> q;
+    q.push( { 0,0 } );
 
-		//Process current state
+    visited[0][0] = true;
+    dp[0][0] = ;
+
+    while(!q.empty()) {
+        int a = q.front().fi;
+        int b = q.front().se;
+        q.pop();
+
+        //----------- Process current state ---------
+        if(a == N)
+            res = max(res, );
 
 
-		/*____________ next states ______________*/
-		int a_,b_,x_;
+        //---------- Next stages ---------
+        int a_,b_,dp_;
 
-		// next state i
-		a_ =  ;
-		b_ =  ;
-		x_ =  ;
+        // Case 1
+        a_ =  ;
+        b_ =  ;
+        dp_ =  dp[a][b] + ;
+        if(!visited[a_][b_] and a_ <= A and b_ <= B) {
+            visited[a_][b_] = true;
+            q.push( {a_, b_} );
+        }
+        dp[a_][b_] = max(dp[a_][b_], dp_); 
 
-		// check visited
-		if(!visited[a_][b_] and a_ <= A and b_ <= B) {
-			visited[a_][b_] = true;
-			q.push( {a_, b_} );
-		}
-		// dp
-		dp[a_][b_] = max(dp[a_][b_], x_); // Only optimization problem (max,min) 
 
-		/*____________ next states by FOR ______________*/
-		FOR(i_,1,N+1) {
-			a_ =  ;
-			b_ =  ;
-			x_ =   ;
+        // Case 2
+        a_ =  ;
+        b_ =  ;
+        dp_ =  dp[a][b] + ;
+        if(!visited[a_][b_] and a_ <= A and b_ <= B) {
+            visited[a_][b_] = true;
+            q.push( {a_, b_} );
+        }
+        dp[a_][b_] = max(dp[a_][b_], dp_);
+        
 
-			// check visited and Stop, Bounding condition
-			if(!visited[a_][i_] and a_ <= A and b_ <= B) {
-				visited[a_][i_] = true;
-				q.push({a_,i_});
-			}
-			// dp
-			dp[a_][i_] = max(dp[a_][i_], x_) // Only optimization problem (max,min)  
-		}
-	}
+        // Cases for
+        FOR() {
+            a_ =  ;
+            b_ =  ;
+            dp_ =  dp[a][b] + ;
+            if(!visited[a_][b_] and a_ <= A and b_ <= B) {
+                visited[a_][b_] = true;
+                q.push( {a_,b_} );
+            }
+            dp[a_][b_] = max(dp[a_][b_], dp_)
+        }
+    }
 
-	//Check and return value
-	FOR()
-		res = max(res, );
-	return dp[][];
+    //Check and return value
+    FOR()
+        res = max(res, );
+    return dp[][];
 }
-
