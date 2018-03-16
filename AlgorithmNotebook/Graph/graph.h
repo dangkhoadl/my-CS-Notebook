702be5aa6758afa12600757c7e0014c0aace0297
dangkhoa1992 <dangledangkhoa@gmail.com>
2018-03-16 11:17:13 +0800

    Fix Graph input

--- AlgorithmNotebook/Graph/graph.h
+++ AlgorithmNotebook/Graph/graph.h
@@ -6,13 +6,13 @@
 // vertex adjvertex weight 
 //Eg:
 //
-//6 6			V E
-//1 2 2			1 --(2)--> 2
-//2 3 2			2 --(2)--> 3
-//2 6 3			2 --(3)--> 6
-//3 4 1			3 --(1)--> 4
-//4 2 3			4 --(3)--> 2
-//5 6 1			5 --(1)--> 6
+//6 6           V E
+//1 2 2         1 --(2)--> 2
+//2 3 2         2 --(2)--> 3
+//2 6 3         2 --(3)--> 6
+//3 4 1         3 --(1)--> 4
+//4 2 3         4 --(3)--> 2
+//5 6 1         5 --(1)--> 6
 
 const int MAXV = 101;
 int V, E;
@@ -20,30 +20,30 @@ vector<int> e[MAXV];
 vector<int> wei[MAXV];
 
 void readInput() {
-	cin >> V >> E;
+    scanf("%lld %lld", &V, &E);     // cin >> V >> E;
 
-	for (int i = 1; i <= V; ++i) {
-		e[i].clear();
-		wei[i].clear();
-	}
-	for (int i = 1; i <= E; ++i) {
-		int v, u, w;
-		cin >> v >> u >> w;
-		e[v].push_back(u);
-		wei[v].push_back(w);
-	}
+    for (int i = 1; i <= V; ++i) {
+        e[i].clear();
+        wei[i].clear();
+    }
+    int v, u, w;
+    for (int i = 1; i <= E; ++i) {
+        scanf("%lld %lld %lld", &v, &u, &w); // cin >> v >> u >> w;
+        e[v].push_back(u);
+        wei[v].push_back(w);
+    }
 }
 /********************************************************************************/
 // V E
-// vertex | numOfAdjVertex	adjvertex[0] weight[0]	adjvertex[1] weight[1]	... 
+// vertex | numOfAdjVertex  adjvertex[0] weight[0]  adjvertex[1] weight[1]  ... 
 //Eg:
 //
-//6 6			V E
-//1 2 2			1 --(2)--> 2
-//2 3 2	6 3		2 --(2)--> 3		2 --(3)--> 6	
-//1 4 1			3 --(1)--> 4
-//1 2 3			4 --(3)--> 2
-//1 6 1			5 --(1)--> 6
+//6 6           V E
+//1 2 2         1 --(2)--> 2
+//2 3 2 6 3     2 --(2)--> 3        2 --(3)--> 6    
+//1 4 1         3 --(1)--> 4
+//1 2 3         4 --(3)--> 2
+//1 6 1         5 --(1)--> 6
 
 const int MAXV = 101;
 int V, E;
@@ -51,20 +51,20 @@ vector<int> e[MAXV];
 vector<int> wei[MAXV];
 
 void readInput() {
-	cin >> V >> E;
-	for (int i = 1; i <= V; ++i) {
-		e[i].clear();
-		wei[i].clear();
-	}
-	for (int i = 1; i <= V; ++i) {
-		int num, u, w;
-		cin >> num;
-		for (int j = 0; j < num; ++j) {
-			cin >> u >> w;
-			e[i].push_back(u);
-			wei[i].push_back(w);
-		}
-	}
+    scanf("%lld %lld", &V, &E);     // cin >> V >> E;
+    for (int i = 1; i <= V; ++i) {
+        e[i].clear();
+        wei[i].clear();
+    }
+    for (int i = 1; i <= V; ++i) {
+        int num, u, w;
+        scanf("%lld", &num); // cin >> num;
+        for (int j = 0; j < num; ++j) {
+            scanf("%lld %lld", &u, &w); // cin >> u >> w;
+            e[i].push_back(u);
+            wei[i].push_back(w);
+        }
+    }
 }
 
 
@@ -73,13 +73,13 @@ void readInput() {
 // vertex adjvertex weight 
 //Eg:
 //
-//6 6			V E
-//1 2 2			1 --(2)-- 2
-//2 3 2			2 --(2)-- 3
-//2 6 3			2 --(3)-- 6
-//3 4 1			3 --(1)-- 4
-//4 2 3			4 --(3)-- 2
-//5 6 1			5 --(1)-- 6
+//6 6           V E
+//1 2 2         1 --(2)-- 2
+//2 3 2         2 --(2)-- 3
+//2 6 3         2 --(3)-- 6
+//3 4 1         3 --(1)-- 4
+//4 2 3         4 --(3)-- 2
+//5 6 1         5 --(1)-- 6
 
 const int MAXV = 101;
 int V, E;
@@ -87,47 +87,47 @@ vector<int> e[MAXV];
 vector<int> wei[MAXV];
 
 void readInput() {
-	cin >> V >> E;
-	for (int i = 1; i <= V; ++i) {
-		e[i].clear();
-		wei[i].clear();
-	}
-	for (int i = 1; i <= E; ++i) {
-		int v, u, w;
-		cin >> v >> u >> w;
-		e[v].push_back(u), e[u].push_back(v);
-		wei[v].push_back(w), wei[u].push_back(w);
-	}
+    scanf("%lld %lld", &V, &E);     // cin >> V >> E;
+    for (int i = 1; i <= V; ++i) {
+        e[i].clear();
+        wei[i].clear();
+    }
+    int v, u, w;
+    for (int i = 1; i <= E; ++i) {
+        scanf("%lld %lld %lld", &v, &u, &w); // cin >> v >> u >> w;
+        e[v].push_back(u), e[u].push_back(v);
+        wei[v].push_back(w), wei[u].push_back(w);
+    }
 }
 /********************************************************************************/
 // V E
-// vertex | numOfAdjVertex	adjvertex[0] weight[0]	adjvertex[1] weight[1]	... 
+// vertex | numOfAdjVertex  adjvertex[0] weight[0]  adjvertex[1] weight[1]  ... 
 //Eg:
 //
-//6 6			V E
-//1 2 2			1 --(2)-- 2
-//2 3 2	6 3		2 --(2)-- 3		2 --(3)-- 6	
-//1 4 1			3 --(1)-- 4
-//1 2 3			4 --(3)-- 2
-//1 6 1			5 --(1)-- 6
+//6 6           V E
+//1 2 2         1 --(2)-- 2
+//2 3 2 6 3     2 --(2)-- 3     2 --(3)-- 6 
+//1 4 1         3 --(1)-- 4
+//1 2 3         4 --(3)-- 2
+//1 6 1         5 --(1)-- 6
 const int MAXV = 101;
 int V, E;
 vector<int> e[MAXV];
 vector<int> wei[MAXV];
 
 void readInput() {
-	cin >> V >> E;
-	for (int i = 1; i <= V; ++i) {
-		e[i].clear();
-		wei[i].clear();
-	}
-	for (int i = 1; i <= V; ++i) {
-		int num, u, w;
-		cin >> num;
-		for (int j = 0; j < num; ++j) {
-			cin >> u >> w;
-			e[i].push_back(u), e[u].push_back(i);
-			wei[i].push_back(w), wei[u].push_back(w);
-		}
-	}
+    scanf("%lld %lld", &V, &E);     // cin >> V >> E;
+    for (int i = 1; i <= V; ++i) {
+        e[i].clear();
+        wei[i].clear();
+    }
+    for (int i = 1; i <= V; ++i) {
+        int num, u, w;
+        scanf("%lld", &num); // cin >> num;
+        for (int j = 0; j < num; ++j) {
+            scanf("%lld %lld", &u, &w); // cin >> u >> w;
+            e[i].push_back(u), e[u].push_back(i);
+            wei[i].push_back(w), wei[u].push_back(w);
+        }
+    }
 }
