7e442a7b2dd2edf69ace071ade1fa95cb18a87c1
dangkhoa1992 <dangledangkhoa@gmail.com>
2018-03-16 10:57:21 +0800

    Fix Bin Search

--- AlgorithmNotebook/Search/UpLowBinSearch.h
+++ AlgorithmNotebook/Search/UpLowBinSearch.h
@@ -1,12 +1,13 @@
 
 
+//      Range: [l, r]
 // Search in a: a[i] <= x 
-
+//         return i
 // 5, 7, 9, 10, 88, 99
 //      search(9) --> a[2] = 9
 //      search(55) --> a[3] = 10
 //      search(4) --> -1
-int binSearchLow(const vector<int> &a, int x, int l, int r) {
+int binSearchPrev(const vector<int> &a, int x, int l, int r) {
     while (true) {
         if (l > r) {
             return l-1;
@@ -25,16 +26,20 @@ int binSearchLow(const vector<int> &a, int x, int l, int r) {
 
 
 
+//      Range: [l, r]
 // Search in a: a[i] >= x
-
+//      return i
 // 5, 7, 9, 10, 88, 99
 //      search(9) --> a[2] = 9
 //      search(55) --> a[4] = 88
-//      search(100) --> 6
-int binSearchUp(const vector<int> &a, int x, int l, int r) {
+//      search(100) --> -1
+int binSearchNext(const vector<int> &a, int x, int l, int r) {
+    int last = r;
     while (true) {
         if (l > r) {
-            return l;
+            if(r != last)
+                return l;
+            return -1;
         }
 
         int m = l + (r-l)/2;
