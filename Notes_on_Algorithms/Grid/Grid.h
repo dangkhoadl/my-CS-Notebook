e6765cf419a5dd298c4789e9526117a3e9c20423
dangkhoadl <dangledangkhoa@gmail.com>
2019-03-19 00:35:35 +0800

    Update Grid

--- Notes_on_Algorithms/Grid/Grid.h
+++ Notes_on_Algorithms/Grid/Grid.h
@@ -1,6 +1,6 @@
 
 /*____________________________________ DIR ____________________________ */
-/*
+/*              [r][c]
 *   up:         -1  0
 *   down:        1  0
 *   left:        0 -1
@@ -10,7 +10,6 @@
 *   down-right   1  1
 *   down-left    1 -1
 */
-
 
 // 8-dir: up, down, left ,right, up-left, up-right, down-right, down-left
 const int DIR = 8;
@@ -36,52 +35,19 @@ int dirC[DIR] = {-1, 1};
 const int DIR = 3;
 int dirR[DIR] = {1,1,0};
 int dirC[DIR] = {0,1,1};
-//_________________________________
+
+
+/*________________________ check Inrange ______________________ */
 // Check if x in [a,b)
 inline bool inRange(int x, int a, int b) {
     return a <= x && x < b; 
 }
-//_________________________________
-bool visited[][];
-int dp[][];
-int bfs() {
-    ms(visited, false);
-    ms(dp, );
 
-    queue<pii> q;
-    q.push({0,0});
-    visited[0][0] = true;
-    dp[0][0] = ;
-    while(!q.empty()) {
-        int r = q.front().fi;
-        int c = q.front().se;
-        int val = dp[r][c];
-        q.pop();
 
-        // Stop condition
-        if(!inRange(r,0,R) || !inRange(c,0,C))
-            continue;
+/*________________________ 1D <-> 2D ______________________ */
+// 2D(n*m) -> 1D
+int cell = i*m + j;
 
-        // Process current state
-        int r_,c_, val_;
-        FOR(i,0,DIR) {
-            r_ = r + dirR[i];
-            c_ = c + dirC[i];
-            val_ = ;
-
-            //check inrange
-            if(!inRange(r_,0,R) || !inRange(c_,0,C))
-                continue;
-
-            if(!visited[r_][c_]) {
-                visited[r_][c_] = true;
-                q.push({r_,c_});
-            }
-            // dp
-            dp[r_][c_] = max();  // max,min, +, - 
-        }
-    }
-
-    // Check and return value
-    return dp[][];
-}
+// 1D -> 2D
+int i = cell / m;
+int j = cell % m;  
