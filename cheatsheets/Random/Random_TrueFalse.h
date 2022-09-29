b3e5b5ddce11d4cdd57f02e5b450050edbaec605
Dang Khoa <dangledangkhoa@gmail.com>
2022-09-29 08:27:28 +0800

    [UPDATE] 29/9/2022

--- cheatsheets/Random/Random_TrueFalse.h
+++
@@ -1,7 +0,0 @@
-
-// Output = True/False
-// Input = Probability of True
-//      Eg: P(true) = 0.8
-bool randTrueFalse(double prob) {
-    return rand() <  prob * (static_cast<double>(RAND_MAX) + 1.0);
-}
